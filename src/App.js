31ef7f937f8f2ddd70c98008632060450089c1e4
Rakib-cloud <ri1641715@gmail.com>
2023-02-19 12:25:02 +0600

    dd

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@
 import './App.css';
 import DataTable from "./Components/DataTable/DataTable";
 import MuiTable from './Components/MuiTable/MuiTable';
-import NewTable from "./Components/NewTable/NewTable";
-import TransportForm from "./Components/TransportForm/TransportForm";
-import VaiTable from './Components/VaiTable/VaiTable';
+
 
 
 function App() {
 
   return (
   <div>
-    {/* <DataTable/> */}
 
-    {/*<NewTable/>*/}
-
-    {/* <TransportForm/> */}
 
     <MuiTable/>
 {/* 
